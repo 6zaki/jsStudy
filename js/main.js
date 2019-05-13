59cc4a5047e38d6705c706be66d86d6b427289f2
6zaki <mutsuzaki@swallow-incubate.com>
2019-05-13 10:01:50 +0900

    5/10分（基礎学習）

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//定数
+//const price = 150;
+
+//変数
+let price = 150;
+
+console.log(price * 140);
+console.log(price * 150);
+
+price = 170;
+console.log(price * 140);
+console.log(price * 150);
